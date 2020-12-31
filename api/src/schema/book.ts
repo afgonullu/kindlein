191ef40638ce13732b838ea5693c1075509931f6
A Faruk Gonullu <af@gonul.lu>
2020-12-31 17:15:49 +0300

    build: api initial configuration
    * initialize node
    * install dependencies
    * configure apollo server with express
    * create sample schema and schema building pattern

---
+++ api/src/schema/book.ts
@@ -0,0 +1,28 @@
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+]
+
+export const bookDefs = `
+  type Book {
+    title: String
+    author: String
+  }
+  
+  type Query {
+    books: [Book]
+  }
+`
+
+export const bookResolvers = {
+  Query: {
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    books: () => books,
+  },
+}
